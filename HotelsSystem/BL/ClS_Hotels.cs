7839d05135d53336e05d76efaa5999ce2b34d811
DllyPink <64475135+DllyPink@users.noreply.github.com>
2022-11-24 19:53:52 +0300

    f

--- HotelsSystem/BL/ClS_Hotels.cs
+++ HotelsSystem/BL/ClS_Hotels.cs
@@ -15,4 +15,9 @@ public class ClS_Hotels
     {
         return await _db.GetGridResult<T, dynamic>("Pro_HotelList", new { Select = SelectPro, HotelTypes = HotelTypes, FullName = FullName, DirectorateID = DirectorateID, WorkPlaceID = WorkPlaceID, PageNumber = PageNumber, PageSize = PageSize, SortColumn = SortColumn, SortDirection = SortDirection, EntryBy = SessionResult.Result }, PageNumber: PageNumber, PageSize: PageSize);
     }
+
+    public async Task<T> InsertUpdateHotels<T>(int SelectPro = 0, int ValID = 0, int HotelTypeID = 0, string HotelName = "", string HotelAddress = "", int StarNumber = 0,int NumberOfRooms=0, int DirectorateID = 0, int WorkPointID = 0, string Note = "")
+    {
+        return await _db.SaveData<T, dynamic>("Pro_InsertUpdateHotels", new { select = SelectPro, ID = ValID, HotelTypeID = HotelTypeID, HotelName = HotelName, HotelAddress = HotelAddress, StarNumber = StarNumber, NumberOfRooms= NumberOfRooms, DirectorateID = DirectorateID, WorkPointID = WorkPointID, Note = Note, EntryBy = SessionResult.Result });
+    }
 }
