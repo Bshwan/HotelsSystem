ed89f2ec225fe723c2f640d146dfbc17ded2d2b9
bshwan <bshwan.mhamad@gmail.com>
2022-11-25 22:13:50 +0300

    [E] roles page delete icon changed to edit, added some routes

--- HotelsSystem/Routes/Routes.cs
+++ HotelsSystem/Routes/Routes.cs
@@ -3,6 +3,9 @@
     public class Routing
     {
         public const string defaultpage = "";
+        public const string roles = "roles";
+        public const string hotels = "hotels";
+        public const string config = "config";
         public const string dashboard = "dashboard";
         public const string userlist = "UserList";
         public const string adduser = "adduser";
