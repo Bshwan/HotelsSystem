eac87814bee40d8eddd692a27f98d2df2563f4be
DllyPink <64475135+DllyPink@users.noreply.github.com>
2022-11-26 20:08:59 +0300

    f

---
+++ HotelsSystem/BL/ClS_Reports.cs
@@ -0,0 +1,22 @@
+﻿using HotelsSystem.Pages.Configs;
+
+namespace HotelsSystem.BL;
+
+public class ClS_Reports
+{
+    private ISqlDataAccess _db { get; set; }
+
+    int SessionValue = 0;
+
+    public ClS_Reports(ISqlDataAccess db, SPResult session)
+    {
+        SessionValue = session.Result;
+        _db = db;
+    }
+    public async Task<PagedResult<T>> SearchList<T>(int SelectPro = 1, int PageNumber = 1, int PageSize = 10,string FullName="",int  DirectorateID=0,int WorkPlaceID=0,int HotelID=0,  int RoomID = 0, int GenderID = 0,  int NationalityID = 0, string FromCheckInDate = "", string ToCheckInDate = "",string FromCheckOutDate="",string ToCheckOutDate="", string SortColumn = "", string SortDirection = "Asc")
+    {
+        return await _db.GetGridResult<T, dynamic>("Pro_SearchList", new { Select = SelectPro, FullName= FullName, DirectorateID = DirectorateID , WorkPlaceID= WorkPlaceID, HotelID= HotelID, RoomID = RoomID, GenderID = GenderID, NationalityID = NationalityID, FromCheckInDate = FromCheckInDate, ToCheckInDate = ToCheckInDate, FromCheckOutDate= FromCheckOutDate, ToCheckOutDate= ToCheckOutDate, PageNumber = PageNumber, PageSize = PageSize, SortColumn = SortColumn, SortDirection = SortDirection, EntryBy = SessionValue }, PageNumber: PageNumber, PageSize: PageSize);
+    }
+
+
+}
