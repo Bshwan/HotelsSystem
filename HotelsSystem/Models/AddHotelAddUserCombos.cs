db2083b8701a8eb72fea6dfea1aae10b419885fb
bshwan <bshwan.mhamad@gmail.com>
2022-11-26 11:04:05 +0300

    [F & E] fixed some errors, added workpoint per user tab in add user, fixed some layout bugs

---
+++ HotelsSystem/Models/AddHotelAddUserCombos.cs
@@ -0,0 +1,6 @@
+namespace HotelsSystem.Models;
+
+public class AddHotelAddUserCombos{
+    public IEnumerable<HotelUserTypeComboBox> HotelUserTypes=Enumerable.Empty<HotelUserTypeComboBox>();
+    public IEnumerable<LanguageInfo> Languages=Enumerable.Empty<LanguageInfo>();
+}
