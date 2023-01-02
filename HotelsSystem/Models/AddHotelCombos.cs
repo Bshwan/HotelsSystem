d559215392a0668607617d98199bacc0b4e6c0c7
bshwan <bshwan.mhamad@gmail.com>
2023-01-02 16:15:06 +0300

    [F] added hotel room config in add hotel dialog

--- HotelsSystem/Models/AddHotelCombos.cs
+++ HotelsSystem/Models/AddHotelCombos.cs
@@ -4,4 +4,6 @@ public class AddHotelCombos{
     public IEnumerable<HotelTypesComboBox> HotelTypes=Enumerable.Empty<HotelTypesComboBox>();
     public IEnumerable<DirectorateInfo> Directorates=Enumerable.Empty<DirectorateInfo>();
     public IEnumerable<WorkingPointInfo> WorkingPoints=Enumerable.Empty<WorkingPointInfo>();
+    public IEnumerable<HotelRoomsTypesInfo> RoomTypes=Enumerable.Empty<HotelRoomsTypesInfo>();
+    public IEnumerable<HotelFloorInfo> FloorTypes=Enumerable.Empty<HotelFloorInfo>();
 }
--- HotelsSystem/Models/HotelRoomsTypesInfo.cs
+++ HotelsSystem/Models/HotelRoomsTypesInfo.cs
@@ -4,4 +4,5 @@ public class HotelRoomsTypesInfo
 {
     public int cfg_HTR_ID { get; set; }
     public string? cfg_HTR_Type { get; set; }
+    public decimal cfg_HTR_Price { get; set; }
 }
